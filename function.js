3e24f6e2741ca03f68a5129fa170d167ec558e35
azrael714991 <118212443+azrael714991@users.noreply.github.com>
2022-12-03 22:51:24 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,181 @@
+// Questions will be asked
+const Questions = [{
+    id: 0,
+    q: "O cimento fosfanato de zinco foi citado pela primeira vez naliteratura em 1879, e a quimica dos produtos comercialmente disponivel hoje em dia foi estabelecida em 1902. É o cimento para cimentação mais antigo e seu longo registo de sucesso de uso clinico o torna padrão de comparação  para outros cimentos. O cimento fosfato de zinco é constituido de um pó e de um liquido que são misturados imediatamente antes de serem usados (Anusavice, 2011). Explique qual seria o melhor método que pode ser usado durante a etapa de manipulação para prolongar o tempo de trabalho do cimento",
+    a: [{ text: "Alguns  sistemas  adesivos a presentam em sua composição a  adição de partículas  orgânicas de tamanho nanométrico com a finalidade de melhorar suas  propriedades  mecânicas ", isCorrect: false },
+        { text: "A  sílica   é a  partícula  mais  usa da  para  a  finalidade  de  melhorar  as  proprie da desmecânicas  dos sistemas  adesivos.", isCorrect: false },
+        { text: "Resfriamento da placa e utilização da maior area possivel ", isCorrect: true },
+        { text: "Retenção menor que os selantes resionosos, com evidência por meio de estudos clinicos controlados randomizados da superioridade dos selantes fluoretados", isCorrect: false }
+    ]
+
+},
+{
+    id: 1,
+    q: "O cimento de óxidio de zinco e eugenol (OZE) é aoresentado por um sistema pó e liquido. O pó contém óxido de zinco e o liquido eugenol. A reação de presa começa quando a água na solução de eugenol hidrolisa o óxido de zinco para formar hidroxio de zinco. O hidroxido de zinco e o eugenol sofrem quelação  e solidificam. Os diversos usos do OZE o tornaram um material bastante versátil, podendo ser usado para cimentação provisória, obturação provisória, bases isolantes térmicas e elétricas e selamento do canal radicular. (Anusavice, 2011) Explique qual a propriedade do OZE representa um beneficio comparado ao cimento ionômero de vidro, que permite ser usado como material de selamento no interior do canal radicular." ,
+    a: [{ text: "Óxido de Zinco e eugenol, porque não permite que a resina polimize", isCorrect: false, isSelected: false },
+        { text: "Hidróxido de Cálcio", isCorrect: false },
+        { text: "Fosfato de Zinco", isCorrect: false },
+        { text: "Propriedade antimicrobiana ", isCorrect: true }
+    ]
+
+},
+{
+    id: 2,
+    q: "As ligas  amálgama odontológico são  geralmente comercializadas na forma de partículas irregulares ou de limalhas produzidas pela moagem ou fresagem de um lingote da liga de amálgama; partículas esféricas produzidas pela atomização da liga liquida em uma câmara preenchida por gás inerte; ou uma mistura de limalhas e esferas (Anusavice, 2011). Leia as afirmativas abaixo e as classifique como certas (V) ou erradas (F). (0,2 cada item) OBS: As respostas, devidamente numeradas, devem ser dadas na Folha resposta 1.As ligas convencionais não produzem a fase Sn7Hg (fase gama 2 - Y2) durante a sua reação de presa de cristalização. 2.(A fase Sn7Hg (gama 2 - Y2) está relacionada com a maior tendência a corrosão e a baixa resistência a compressão do amálgama. ( ) 3. As ligas com alto teor de cobre apresentam melhores propriedades mecânicas e necessitam de uma quantidade menor de mercúrio para molhamento das partículas da liga do que as ligas de baixo teor de cobre. ( ) 4. A expansão tardia do amálgama é observada em ligas contendo estanho quando sofrem contaminação por umidade durante a etapa de trituração ou condensação. 5. A corrosão do amálgama libera óxidos que se depositam na interface dente/amálgama selando essa interface e evitando a infiltração marginal. ( ) ",
+    a: [{ text: "F-V-V-F-F", isCorrect: false },
+        { text: "V-V-F-F-V", isCorrect: false },
+        { text: "F-V-V-F-V", isCorrect: true },
+        { text: "V-V-F-V-F", isCorrect: false }
+    ]
+
+},
+
+{
+    id: 3,
+    
+      
+    q:'A odontologia minimamente invasiva tem por intuito propiciar meios para a remineralização do tecido dentário lesionado por cáfil através da utilização de materiais odontológicos com excelentes propriedades, impedindo a progressão de lesões iniciais e, consequentemente, preconizando a promoção da manutenção e longevidade da estrutura dental sadia. Em função disso, o cimento de ionômero de vidro (CIV) têm sido utilizado constantemente no âmbito odontológico devido às suas vantajosas propriedades clínicas, embora apresente algumas propriedades negativas. Resultados iniciais usando o CIV convencional demonstraram a viabilidade desse cimento na Técnica Restauradora Atraumática (TRA), o que levou ao desenvolvimento do Cimento lonômero de Vidro de Alta Viscosidade. (Anusavice, 2011).  Explique, bascado nas características e propriedades do material, porque o cimento ionômero de vidro de alta viscosidade pode ser usado como material restaurador definitivo n técnica do TRA. ',
+    a: [{ text: "Particulas menores que os ionômeros convencionais ", isCorrect: false },
+        { text: "Dureza superficial maior que os ionômetros convencionais", isCorrect: false },
+        { text: "Cimento de alta viscisidade apresentada maior resistencia a compressão, pois contem particulas menores e uma maior relação ", isCorrect: true },
+        { text: "Ácido liofizado agregado ao pó", isCorrect: false }
+    ]
+
+},
+
+{
+    id: 4,
+    q:'O compósito odontológico ou resina composta é um material polimérico de alta densidade de ligações cruzadas bastante usado na odontologia com a finalidade de restaurar (estética e funcionalmente) os dentes. Sua aplicação é bem ampla, podendo ser usado para mudar a forma dos dentes, restaurar dentes fraturados, preencher cavidades provocadas pela cárie e minimizar imperfeições do esmalte dentário. (Anusavice, 2011). Em relação ás resinas compostas, responda as questões abaixo: ( 0,5 cada item) a) Quais os componentes essenciais dos materiais compósitos restauradores b) Qual a relação do conteúdo de carga com a absorção de água na resina.',
+
+    a: [{ text: "A. Presentam como vantagem a simplificação B. A inserção da carga de conteudo menos valorizados " , isCorrect: false },
+        { text: "A. Matriz da carga e agente da resina. B. Quanto maior o conteúdo de carga maior a absorção de agua da resina", isCorrect: false },
+        { text: "A. Matriz Carga e agente da união B. Quanto maior o conteúdo de carga menor a absorção de agua da resina", isCorrect: true },
+        { text: "A. Agente da união e carga da matriz. B. Quanto maior o conteúdo de carga maior a absorção ", isCorrect: false }
+    ]
+
+},
+
+{
+    id: 5,
+    q:'Com os novos conceitos de preservação da estrutura dentária, simplificação do procedimento restaurador e de adesividade, deseja-se atual ente que o material utilizado para restaurar a cavidade não prejudique as características biológicas do dente. Para isso, são utilizados o materiais protetores do complexo dentinopular. Tradicionalmente esses materiais se dividem em: agentes de selamento, Em relação às resinas compostas, responda as questões abaixo: (0,5 cada item - Total 2,0 pontos) forramento e base cavitária ( Reis e Loguércio 2007)  a) Qual o primeiro critério para ser utilizado na escolha do material para proteção do complexo dentinopulpar?  b) Explique, bascado nas propriedades do material, porque o hidróxido de cálcio não pode ficar exposto na cavidade oral.  c) Porque deve-se aguardar 7 min para inserção da resina em cima do Cimento ionômero de vidro d) Explique porque o cimento fosfato de zinco não pode ser utilizado para proteção do complexo dentinopulpar',
+    a: [{ text: "A. A profundidade da cavidade ou ionômetro B. Porque tem alta solubidade em cimento quando comparado a agua. C. Para melhorar a adesão. D. Reação iômerica  ", isCorrect: false },
+        { text: "A. Cavidade da profundidade. B. Solubidade em alta comparado a agua. C. Para melhorar a intensidade. D. Reação anestesica", isCorrect: false },
+        { text: "A. Profundidade da cavidade ou quantidade de dentina remanescente. B. Porque tem alta solubidade em agua quando comparado a outros cimentos. C. Para melhor adesão ao dente. D. Reação exotérica  ", isCorrect: true },
+        { text: "A Profundidade da cavidade. B. A solubidade comparada a agua. C. Para melhorar o desempenho. D. Reação Quimica", isCorrect: false }
+    ]
+
+},
+
+
+
+
+
+]
+
+// Set start
+var start = true;
+
+// Iterate
+function iterate(id) {
+
+// Getting the result display section
+var result = document.getElementsByClassName("result");
+result[0].innerText = "";
+
+// Getting the question
+const question = document.getElementById("question");
+
+
+// Setting the question text
+question.innerText = Questions[id].q;
+
+// Getting the options
+const op1 = document.getElementById('op1');
+const op2 = document.getElementById('op2');
+const op3 = document.getElementById('op3');
+const op4 = document.getElementById('op4');
+
+
+// Providing option text 
+op1.innerText = Questions[id].a[0].text;
+op2.innerText = Questions[id].a[1].text;
+op3.innerText = Questions[id].a[2].text;
+op4.innerText = Questions[id].a[3].text;
+
+
+// Providing the true or false value to the options
+op1.value = Questions[id].a[0].isCorrect;
+op2.value = Questions[id].a[1].isCorrect;
+op3.value = Questions[id].a[2].isCorrect;
+op4.value = Questions[id].a[3].isCorrect;
+
+var selected = "";
+
+// Show selection for op1
+op1.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightgoldenrodyellow";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightskyblue";
+    selected = op1.value;
+})
+
+// Show selection for op2
+op2.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightgoldenrodyellow";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightskyblue";
+    selected = op2.value;
+})
+
+// Show selection for op3
+op3.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightgoldenrodyellow";
+    op4.style.backgroundColor = "lightskyblue";
+    selected = op3.value;
+})
+
+// Show selection for op4
+op4.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightgoldenrodyellow";
+    selected = op4.value;
+})
+
+// Grabbing the evaluate button
+const evaluate = document.getElementsByClassName("evaluate");
+
+// Evaluate method
+evaluate[0].addEventListener("click", () => {
+    if (selected == "true") {
+        result[0].innerHTML = "Correto";
+        result[0].style.color = "green";
+    } else {
+        result[0].innerHTML = "Errado";
+        result[0].style.color = "red";
+    }
+})
+}
+
+if (start) {
+iterate("0");
+}
+
+// Next button and method
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+
+next.addEventListener("click", () => {
+start = false;
+if (id < 5) {
+    id++;
+    iterate(id);
+    console.log(id);
+}
+
+})
